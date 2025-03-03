dff34caf10c0c795e3344651e0a46463ba1b7ff4
Bangoura7 <bangsabdallah4@gmail.com>
2025-03-03 15:43:26 +0000

    l'ajout des views login et logout

--- rcm_b/website_rcm/urls.py
+++ rcm_b/website_rcm/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     
 
 ]
--- rcm_b/website_rcm/views.py
+++ rcm_b/website_rcm/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def home(request): 
     return render(request, 'home.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    return render(request, 'logout.html', {})
 
