b59811531ef90b340e221c97abcfbad47ff20073
Bangoura7 <bangsabdallah4@gmail.com>
2025-03-24 10:43:45 +0000

    l'ajout de la page d'inscription

--- rcm_b/website_rcm/forms.py
+++ rcm_b/website_rcm/forms.py
@@ -4,18 +4,19 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label = "", widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Addresse'}))
-    first_name = forms.CharField(label = "", max_length=100, widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First name'}))
-    last_name = forms.CharField(label = "", max_length=100,  widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last name'}))
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
-    class Meta:
-        model = User
-        fields = {'username', 'first_name', 'last_name', 'email', 'password1', 'password2'}
 
-    
-    def __init__(self, *args, **kwargs):
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+	def __init__(self, *args, **kwargs):
             super(SignUpForm, self).__init__(*args, **kwargs)
-            
+
             self.fields['username'].widget.attrs['class'] = 'form-control'
             self.fields['username'].widget.attrs['placeholder'] = 'User Name'
             self.fields['username'].label = ''
@@ -30,3 +31,5 @@ class SignUpForm(UserCreationForm):
             self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
             self.fields['password2'].label = ''
             self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
